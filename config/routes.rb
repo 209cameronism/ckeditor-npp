bb8629b57b7116559ea8508858a0bc49fb604847
209cameronism <209cameronism@users.noreply.github.com>
2017-06-01 16:19:55 +0800

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Ckeditor::Engine.routes.draw do
   resources :pictures, only: [:index, :create, :destroy]
   resources :attachment_files, only: [:index, :create, :destroy]
   
-    match "/uploads/ckeditor/picture/data/:id/:basename.:extension", :controller => "pictures", :action => "download_ck_pic", via: :get
+  match "/uploads/ckeditor/picture/data/:id/:basename.:extension", :controller => "pictures", :action => "download_ck_pic", via: :get
 end
